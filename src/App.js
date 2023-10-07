db72c54a61fe13a0547b604099f2e59a8aaed07c
Babar Ali <144090286+babarali267@users.noreply.github.com>
2023-10-07 13:57:21 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,57 @@
-import React from "react"
+
+import { useState } from "react"
+import quizData from "./compoent/question"
+
 import './style.css'
-// function Navbar (){
-//      return(<>
-//        <h1>NAVBAR</h1>
-//      </>)
+
+const App =()=>{
+  const [currentQuestion,setcurrentQuestion]= useState(0)
+  const [userAnswer,setUserAnser] = useState([])
+  const [score,setScore] = useState(0)
+  const handelAnsweClick = (selectoption) =>{
+     const isCorrect = quizData[currentQuestion].correctAnswer === selectoption
+
+      setUserAnser([ ...userAnswer, {question : currentQuestion,answer: selectoption}])
+      if(isCorrect){
+        setScore(score + 1)
+      }
+
+      setcurrentQuestion( currentQuestion + 1)
+  }
+
+  const restQuize = ()=>{
+     setcurrentQuestion(0)
+     setUserAnser([])
+     setScore(0)
+  }
 
 
-// }
- 
-// export class Car extends React.Component{
-//      render(){
-//          return <h1>Class Comp</h1>
-//      }
-// }
 
-// props 
+  return(<>
+      <div className="quize">
+       
+         {
+           currentQuestion < quizData.length ? (
+               <div>
+                   <h2>{quizData[currentQuestion].question}</h2>
+                   <ul>
+                     {quizData[currentQuestion].options.map((option,index)=>(
+                       <li key={index} onClick={()=>handelAnsweClick(option)} >{option}</li>
+                     ))}
+                   </ul>
+              </div>
+           ):(
+             <div className="complete">
+               <h2>quize Completed !</h2>
+               <p>Your Score {score} / {quizData.length}</p>
+               <button onClick={restQuize}>Reset</button>
+            </div>
+           )
+         }
+      </div>
+  </>)
+}
 
 
-// export const Navbar =()=>{
-//      return(<>
-//           <h1>NAVBAR   </h1>
-         
-//      </>)
-// }
 
- const Navbar =() =>{
-     return(<>
-          <h1>NAVBAR   </h1>
-         
-     </>)
-
-     }
-
-export default Navbar
+export default App
